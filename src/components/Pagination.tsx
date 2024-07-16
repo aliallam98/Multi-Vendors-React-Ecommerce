c56e30ce4398865aa6f30a15c7ce667893a92b4b
aliallam98 <allam98.ali@gmail.com>
2024-07-17 01:08:12 +0300

    Edited pagination component ui .

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,7 +1,8 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 
 import { Button } from "./ui/button";
-import { formUrlQuery } from "@/lib/utils";
+import { cn, formUrlQuery } from "@/lib/utils";
+import { MoveLeft, MoveRight } from "lucide-react";
 
 interface IProps {
   page: string | number;
@@ -10,7 +11,7 @@ interface IProps {
 
 const Pagination = ({ page, totalPages }: IProps) => {
   const navigate = useNavigate();
-  const searchParams = useSearchParams();
+  const [searchParams] = useSearchParams()
 
   const onClickHandler = (value: string) => {
     const pageValue = value === "prev" ? Number(page) - 1 : Number(page) + 1;
@@ -25,21 +26,32 @@ const Pagination = ({ page, totalPages }: IProps) => {
   return (
     <div className="flex justify-center items-center gap-x-10 mt-10">
       <Button
+        className="rounded-full size-12 bg-transparent border-2 border-lightBlueColor"
         variant={"outline"}
         size={"sm"}
         onClick={() => onClickHandler("prev")}
         disabled={Number(page) <= 1}
       >
-        Prev
+        <MoveLeft />
       </Button>
-      <p>{`${page}/${totalPages}`}</p>
+      <div className="flex gap-2">
+        {[...Array(totalPages)].map((_, i) => (
+          <span key={i} className={cn("size-10 border-2 border-[#e5e3da] flex justify-center items-center rounded-full",
+          page == i+1 && "bg-lightBlueColor "
+          )}>
+            {i + 1}
+          </span>
+        ))}
+      </div>
+
       <Button
+        className="rounded-full size-12 bg-transparent border-2 border-lightBlueColor cursor-pointer"
         variant={"outline"}
         size={"sm"}
         onClick={() => onClickHandler("next")}
         disabled={Number(page) >= Number(totalPages)}
       >
-        Next
+        <MoveRight />
       </Button>
     </div>
   );
